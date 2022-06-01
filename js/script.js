70f9c2b771e8047d7f03282c21ec4845b052a3f2
LorisP96 <lorispalazzo96@gmail.com>
2022-06-01 15:45:47 +0200

    tolto spazio tra metodo e funzione

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // quanti chilometri?
-let userKm = parseInt ( prompt ('quanti chilometri percorrerete?') );
+let userKm = parseInt( prompt('quanti chilometri percorrerete?') );
 // quanti anni?
-let userAge = parseInt ( prompt ('quanti anni ha?') );
+let userAge = parseInt( prompt('quanti anni ha?') );
 // costo biglietto fisso
 let userPrice = userKm * 0.21;
 // sconto
@@ -9,10 +9,10 @@ let userDiscPrice;
 
 let userMessage;
 
-if ( userAge > 65 ) {
+if (userAge > 65) {
     userDiscPrice = userPrice - (userPrice * 0.4);
     userMessage = "sconto applicato del 40%, il costo del suo biglietto è di" + ' ' + userDiscPrice.toFixed(2) + '€'; 
-}   else if (userAge < 18 ) {
+}   else if (userAge < 18) {
     userDiscPrice = userPrice - (userPrice * 0.2);
     userMessage = "sconto applicato del 20%, il costo del suo biglietto è di" + ' ' + userDiscPrice.toFixed(2) + '€'; 
 } else {
